ed6170d3df06bd5dc17b98b091f81698643e762c
MenaVillalobos <jimee69styles@gmail.com>
2023-11-15 23:09:29 -0600

    idk

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const rootElement = document.getElementById("root");
 const App = (props) => {
     return <>
         <h1>Setting the same as in the example</h1>
-        <h2 className="counter">{props.initialCounter}</h2>
+        <h2 className='counter'>{props.initialCounter}</h2>
     </>
 }
 
