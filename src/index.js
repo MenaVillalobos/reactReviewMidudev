80a429998aa1ea560ac5f56b769847e35659ff42
MenaVillalobos <jimee69styles@gmail.com>
2023-11-15 22:59:49 -0600

    passing props example

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './index.css'
 
 const rootElement = document.getElementById("root");
 
 const App = (props) => {
-    return <h1>Setting the same as in the example</h1>
+    return <>
+        <h1>Setting the same as in the example</h1>
+        <h2 className="counter">{props.initialCounter}</h2>
+    </>
 }
 
 ReactDOM.render(
-    <App/>,
+    <App initialCounter={0}/>,
     rootElement
 )
