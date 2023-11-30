e0ad782427fa0360879c754f5a3101316c3670d6
MenaVillalobos <jimee69styles@gmail.com>
2023-11-30 16:02:19 -0600

    spread Op for complex states

--- src/index.js
+++ src/index.js
@@ -11,42 +11,57 @@ const App = () => {
 
     const [counters, setCounters] = useState({
         left: 0,
-        right: 0
+        right: 0,
+        clicked: 0,
+        message: 'State message'
     })
 
     const handleLeftClick = () => {
-        setCounters({
+        const newCountersState = {
+            ...counters,
             left: counters.left + 1,
-            right: counters.right
-        })
+            clicked: counters.clicked + 1
+        }
+        setCounters(newCountersState);
     }
 
     const handleRightClick = () => {
         setCounters({
-            left: counters.left,
-            right: counters.right +1
+            ...counters,
+            right: counters.right +1,
+            clicked: counters.clicked + 1
         })
     }
 
     return (
         <>
             <div className='gralContainer'>
-                <div className='leftContainer'>
-                    <h2 className='leftScore' >
-                        {counters.left}
-                    </h2>
-                    <button className='leftBtn' onClick={ handleLeftClick }>
-                        LEFT
-                    </button>
-                </div>
+                <div className='countersContainer'>
+                    <div className='leftContainer'>
+                        <h2 className='leftScore' >
+                            {counters.left}
+                        </h2>
+                        <button className='leftBtn' onClick={ handleLeftClick }>
+                            LEFT
+                        </button>
+                    </div>
 
-                <div className='rightContainer'>
-                    <button className='rightBtn' onClick={ handleRightClick }>
-                        RIGHT
-                    </button>
-                    <h2 className='rightScore' >
-                        {counters.right}
-                    </h2>
+                    <div className='rightContainer'>
+                        <button className='rightBtn' onClick={ handleRightClick }>
+                            RIGHT
+                        </button>
+                        <h2 className='rightScore' >
+                            {counters.right}
+                        </h2>
+                    </div>
+                </div>
+                <div className='msgsContainer'>
+                    <p>
+                        Clicked {counters.clicked} times
+                    </p>
+                    <p>
+                        {counters.message}
+                    </p>
                 </div>
             </div>
         </>
